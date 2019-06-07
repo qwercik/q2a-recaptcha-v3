ba05a2514f6444620d4c3bb283e5a9a2fa4670b8
qwercik <erykandrzejewski@gmail.com>
2019-06-07 13:09:54 +0200

    Remove unneccessary comments

--- src/lang/qa-recaptcha-captcha-lang-pl.php
+++ src/lang/qa-recaptcha-captcha-lang-pl.php
@@ -1,23 +1,4 @@
 <?php
-/*
-    Question2Answer by Gideon Greenspan and contributors
-    http://www.question2answer.org/
-
-    Description: Language phrases for reCAPTCHA captcha module plugin
-
-
-    This program is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public License
-    as published by the Free Software Foundation; either version 2
-    of the License, or (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    More about this license: http://www.question2answer.org/license.php
-*/
 
 return [
     'must_sign_up' => 'Musisz $1 zarejestrować się $2, aby móc używać usługi reCAPTCHA.',
--- src/qa-recaptcha-captcha.php
+++ src/qa-recaptcha-captcha.php
@@ -1,28 +1,9 @@
 <?php
-/*
-    Question2Answer by Gideon Greenspan and contributors
-    http://www.question2answer.org/
-
-    File: qa-plugin/recaptcha-captcha/qa-recaptcha-captcha.php
-    Description: Captcha module for reCAPTCHA
-
-
-    This program is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public License
-    as published by the Free Software Foundation; either version 2
-    of the License, or (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    More about this license: http://www.question2answer.org/license.php
-*/
 
 require_once __DIR__ . '/recaptchalib.php';
 require_once __DIR__ . '/admin-form.php';
 require_once __DIR__ . '/user-verifier.php';
+
 
 class qa_recaptcha_captcha
 {
